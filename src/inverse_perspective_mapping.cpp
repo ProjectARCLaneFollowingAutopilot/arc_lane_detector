aa5db4841c00f41c7ed656db466be1b356b6e0fc
NikkukkiN <nikku_alatur@yahoo.de>
2017-04-21 16:56:20 +0200

    Reading out pixel values works properly

--- src/inverse_perspective_mapping.cpp
+++ src/inverse_perspective_mapping.cpp
@@ -4,6 +4,7 @@
 IPM::IPM()
 {
   std::cout<<"IPM object created!"<<std::endl;
+  connection_vector_ = new connection_struct[2];
 }
 
 // Default destructor.
@@ -22,8 +23,8 @@ void IPM::setParam(float camera_height_m, float pitch_angle_deg, int focal_lengt
   this->input_height_px_ = input_height_px;
   this->num_px_ = (this->input_height_px_)*(this->input_width_px_);
 
-  //delete[] connection_vector_;
-  //connection_vector_ = new connection_struct[this->num_px_];
+  delete[] connection_vector_;
+  connection_vector_ = new connection_struct[this->num_px_];
 
 }
 
@@ -36,5 +37,26 @@ void IPM::getImage(cv::Mat src)
 // Method which does IPM and returns undistorted, projected image.
 cv::Mat IPM::invPerspectiveMapping()
 {
+  // Iterate trough all pixel points of the input image to generate the connection_vector_.
+  int counter = 0;
+  for(int i_rows = 0; i_rows<input_height_px_; i_rows++)
+  {
+    for(int i_cols; i_cols<input_width_px_; i_cols++)
+    {
+      // Set Index in connection_vector_.
+      // Set RGB values of input image in connection_vector_.
+      this->connection_vector_[counter].blue = (int)(this->input_img_).at<cv::Vec3b>(i_rows,i_cols)[0];
+      this->connection_vector_[counter].green = (int)(this->input_img_).at<cv::Vec3b>(i_rows,i_cols)[1];
+      this->connection_vector_[counter].red = (int)(this->input_img_).at<cv::Vec3b>(i_rows,i_cols)[2];
+      // Set pixel position of corresponding input image in connection_vector_.
+      this->connection_vector_[counter].input_u = i_cols;
+      this->connection_vector_[counter].input_v = i_rows;
+      // Find lambda from equation 7.
+      // Find and set x_ground, y_ground, z_ground=0 in connection_vector_.
+    }
+
+  }
+
+  // Discretize the points from connection_vector_ onto a new image.
 
 }
