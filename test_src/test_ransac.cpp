722feaeeb248a6f654ddbc9a0a9eecb4355064f5
NikkukkiN <nikku_alatur@yahoo.de>
2017-06-05 13:17:10 +0200

    Executable for testing RANSAC added

---
+++ test_src/test_ransac.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	// Read in an image.
+
+	// Define manually where the two lane boundaries are --> save rho and theta of the lines.
+
+	// Create a Region around the two lines (Reactangle, parallel to line).
+
+	// Assign all the points inside the two rectangles to two vectors.
+
+	// Fit a polynomial to the left data set by using the RANSAC class and do the same for right.
+
+	// Draw the polynomial in a high resolution onto the original image (only for the ROI).
+
+	return 0;
+}
