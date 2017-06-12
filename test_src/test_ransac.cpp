b757138ff33c4a8305af3cec00d9d4b41077d614
NikkukkiN <nikku_alatur@yahoo.de>
2017-06-12 23:46:22 +0200

    Working RANSAC AND GOOD!

--- test_src/test_ransac.cpp
+++ test_src/test_ransac.cpp
@@ -1,24 +1,43 @@
+
+#include <fstream>
 #include <iostream>
 #include <cv.h>
+#include <vector>
 #include "opencv2/highgui/highgui.hpp"
+#include "../../arc_lane_tracking_tools/include/ransac_fitting/ransac_fitting.hpp"
 
-using namespace std;
+using namespace cv;
 
 int main()
 {
 
+	Ransac test_kurve;
+
 	// TEST.
-	// Create a data set: Sample a polynomial of 2nd degree in a fine mesh.
+	// Read in the generated data set.
+	vector<Point2f> datenset;
+	std::ifstream read_in;
+	read_in.open("/home/nikhilesh/DataTextFiles/data.txt");
+	while(read_in)
+	{
+		float x;
+		float y_noise;
+		read_in>>x>>y_noise;
+		Point2f temp_point(x, y_noise);
+		datenset.push_back(temp_point);
+	}
+	read_in.close();
 
-
-
-
-
-	// Modify the data set: Add some noise and outliers to it.
-	// Create a RANSAC object.
-	// Set parameters for RANSAC.
-	// Set data set for RANSAC.
-	// Let RANSAC run and get the coefficients.
-
+	// Assign the data set.
+	test_kurve.Ransac::setRansacDataSet(datenset);
+	// Set the RANSAC parameters.
+	test_kurve.Ransac::setRansacParams(0.5, 20, 3);
+	// Get the coefficients.
+	vector<float> found_coeffs = test_kurve.Ransac::getRansacCoeff();
+	std::cout<<"a: "<<found_coeffs[0]<<std::endl;
+	std::cout<<"b: "<<found_coeffs[1]<<std::endl;
+	std::cout<<"c: "<<found_coeffs[2]<<std::endl;
+	
+	
 	return 0;
 }
