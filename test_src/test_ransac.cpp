0d3cb839ee06aa36f023c2bf03589bdd3f1758d3
NikkukkiN <nikku_alatur@yahoo.de>
2017-06-12 13:15:38 +0200

    Nix

--- test_src/test_ransac.cpp
+++ test_src/test_ransac.cpp
@@ -6,9 +6,19 @@ using namespace std;
 
 int main()
 {
-	// Create a data set.
 
-	// 
+	// TEST.
+	// Create a data set: Sample a polynomial of 2nd degree in a fine mesh.
+
+
+
+
+
+	// Modify the data set: Add some noise and outliers to it.
+	// Create a RANSAC object.
+	// Set parameters for RANSAC.
+	// Set data set for RANSAC.
+	// Let RANSAC run and get the coefficients.
 
 	return 0;
 }
