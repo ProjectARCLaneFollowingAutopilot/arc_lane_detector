34267b2caeaba7e7efd69638ffe9a5050ca5f2ba
NikkukkiN <nikku_alatur@yahoo.de>
2017-06-13 10:48:35 +0200

    New

--- test_src/test_ransac.cpp
+++ test_src/test_ransac.cpp
@@ -31,13 +31,18 @@ int main()
 	// Assign the data set.
 	test_kurve.Ransac::setRansacDataSet(datenset);
 	// Set the RANSAC parameters.
-	test_kurve.Ransac::setRansacParams(0.5, 20, 3);
+	test_kurve.Ransac::setRansacParams(0.5, 50, 3);
 	// Get the coefficients.
+	// t for measuring the performance of the algorithm.
+	double t = (double)getTickCount();
+
 	vector<float> found_coeffs = test_kurve.Ransac::getRansacCoeff();
 	std::cout<<"a: "<<found_coeffs[0]<<std::endl;
 	std::cout<<"b: "<<found_coeffs[1]<<std::endl;
 	std::cout<<"c: "<<found_coeffs[2]<<std::endl;
-	
+	t = ((double)getTickCount() - t)/getTickFrequency();
+
+	std::cout<<"Time passed in seconds: "<<t<<std::endl;
 	
 	return 0;
 }
